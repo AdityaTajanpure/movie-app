eca78509a3d809927787ed26d54a5fcaaec10d2d
AdityaTajanpure <adityat@accucia.co>
2022-05-22 12:04:19 +0530

    added cors

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+var allowedDomains = [
+  "https://guvi-movie-app.netlify.app/",
+  "http://localhost:3000",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedDomains.indexOf(origin) === -1) {
+        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 MongoConnection.connect();
 
